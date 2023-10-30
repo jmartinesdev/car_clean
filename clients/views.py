7853652d036a3f2d3c0aacfe499c4f5296170a5b
Jakeline Martines <jakeline_13_martines@hotmail.com>
2023-10-30 15:06:36 +0000

    def created to register all the information in database/js onclick created to add a new field input to add new car

--- clients/views.py
+++ clients/views.py
@@ -3,4 +3,22 @@ from django.http import HttpResponse
 
 # Create your views here.
 def clients(request):
-    return render(request, 'clients.html')
+    if request.method == "GET":
+        return render(request, 'clients.html')
+    elif request.method == "POST":
+        name = request.POST.get("name")
+        surname = request.POST.get("surname")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+        cars = request.POST.getlist('car')
+        regCar = request.POST.getlist('reg-car')
+        year = request.POST.getlist('year')
+        
+
+        
+        print(name)
+        print(cars)
+        print(regCar)
+        print(year)
+        
+        return HttpResponse('teste')
--- templates/static/clients/js/clients.js
+++ templates/static/clients/js/clients.js
@@ -1,7 +1,7 @@
 function addCar(){
     container = document.getElementById("form-car")
 
-    html = '<br><div class="row"> <div class="col-md"> <input type="text" placeholder="car" class="form-control" name=""></div> <div class="col-md"><input type="text" placeholder="reg-car" class="form-control"> </div> <div class="col-md"> <input type="number" placeholder="year" class="form-control" name="ano">'
+    html = '<br><div class="row"> <div class="col-md"> <input type="text" placeholder="car" class="form-control" name="car"></div> <div class="col-md"> <input type="text" placeholder="reg-car" class="form-control" name="reg-car"> </div> <div class="col-md"> <input type="number" placeholder="year" class="form-control" name="year">'
 
     container.innerHTML += html
 }
