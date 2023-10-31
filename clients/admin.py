18f7fe679647b42c3ca8a0c4e49726bf22ce2d18
Jakeline Martines <jakeline_13_martines@hotmail.com>
2023-10-31 19:19:48 +0000

    add my models with the class Clients and Cars and import to the admin.py to manage my database

--- clients/admin.py
+++ clients/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Clients, Cars
 
 # Register your models here.
+admin.site.register(Clients)
+admin.site.register(Cars)
--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Clients(models.Model):
+    name = models.CharField(max_length=50)
+    surname = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=10)
+    
+    def __str__(self) -> str:
+        return self.name
+    
+class Cars(models.Model):
+    car = models.CharField(max_length=20)
+    regCar = models.CharField(max_length=8)
+    yearCar = models.IntegerField()
+    client = models.ForeignKey(Clients, on_delete=models.CASCADE)
+    
+    def __str__(self) -> str:
+        return self.car
