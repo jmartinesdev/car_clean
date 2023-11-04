04c01ee5f1f6fc0f68c826523d186a841a4edc50
Jakeline Martines <jakeline_13_martines@hotmail.com>
2023-11-04 02:16:16 +0000

    Refactor urls.py for clarity and remove redundant redirect

--- clients/urls.py
+++ clients/urls.py
@@ -1,9 +1,6 @@
-from django.urls import path, include
-from django.contrib import admin
-from django.views.generic.base import RedirectView
+from django.urls import path
+from clients import views
 
 urlpatterns = [
-    path('', RedirectView.as_view(url='clients/')),
-    path('admin/', admin.site.urls),
-    path('clients/', include('clients.urls')),
+    path('', views.clients, name='clients')
 ]
